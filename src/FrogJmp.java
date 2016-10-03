c5ff80cbdc4054864143990c73d9576ab5228e6c
seojaeryong <seojaeryong@gmail.com>
2016-10-03 23:17:50 +0900

    add FrogJmp

---
+++ src/FrogJmp.java
@@ -0,0 +1,10 @@
+
+public class FrogJmp {
+	public int solution(int X, int Y, int D) {
+		/*
+		 *  Task Description
+		 *  https://codility.com/programmers/task/frog_jmp/
+		 */
+		return (int)Math.ceil((double)(Y-X)/D);
+    }
+}
