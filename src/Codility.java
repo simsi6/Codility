83b8baa8e042bb277ef35b23ea033edafc67e548
seojaeryong <seojaeryong@gmail.com>
2016-10-04 21:52:08 +0900

    add CyclicRotation

--- src/Codility.java
+++ src/Codility.java
@@ -6,7 +6,8 @@ public class Codility {
 		//System.out.println(new BinaryGap().solution(1041));
 		//System.out.println(new TapeEquilibrium().solution(new int[]{3,1,2,4,3}));
 		//System.out.println(new FrogJmp().solution(10, 85, 30));
-		System.out.println(new PermMissingElem().solution(new int[]{2,3,1,5}));
+		//System.out.println(new PermMissingElem().solution(new int[]{2,3,1,5}));
+		System.out.println(new CyclicRotation().solution(new int[]{3,8,9,7,6}, 3));
 	}
 
 }
---
+++ src/CyclicRotation.java
@@ -0,0 +1,26 @@
+
+public class CyclicRotation {
+	public int[] solution(int[] A, int K) {
+        // write your code in Java SE 8
+		int N = A.length;
+		if(N <= 1 || K == 0) {
+			return A;
+		}
+		
+		int shift = K % N;
+		if(shift == 0) {
+			return A;
+		}
+		
+		int pivot = N - shift;
+		int[] result = new int[N];
+		for(int i = 0 ; i<shift ; i++) {
+			result[i] = A[pivot+i];
+		}
+		for(int i=shift ; i<N ; i++) {
+			result[i] = A[i-shift];
+		}
+		
+		return result;
+    }
+}
